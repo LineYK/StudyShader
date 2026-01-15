9da094005826fa84c8cc78a6dbb93c4679a0d6bb
LineYK <yhoung11@gmail.com>
2026-01-16 00:56:52 +0900

    노말 벡터 및 벡터 정규화

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -8,7 +8,7 @@ void ofApp::setup(){
 	ofEnableDepthTest();
 
 	torusMesh.load("torus.ply");
-	uvShader.load("passthrough.vert", "uv_vis.frag");
+	uvShader.load("mesh.vert", "normal_vis.frag");
 }
 
 //--------------------------------------------------------------
